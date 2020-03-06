96a02ab53c41ca91a9b52969ac72be5f543575c4
Yaremith  Damian <laudamian27@gmail.com>
2020-03-05 19:24:11 -0600

    first comit

---
+++ app/src/main/java/com/example/listviewfruits/Fruta.kt
@@ -0,0 +1,11 @@
+package com.example.listviewfruits
+
+class Fruta(nombre:String, Imagen:Int) {
+    var nombre:String =""
+    var Imagen:Int = 0
+
+    init{
+        this.nombre = nombre
+        this.Imagen = Imagen
+    }
+}
